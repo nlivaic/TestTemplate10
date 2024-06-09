d423a22db9438b5bfa493f42eb9921e0110ef7d3
Nenad Livaic <nenad@revelator.com>
2024-06-09 13:46:29 +0200

    New task in release pipeline: secrets . [+semver: minor]

--- deployment/iac.bicep
+++ deployment/iac.bicep
@@ -4,7 +4,6 @@ param sql_admin_username string
 @secure()
 param sql_admin_password string
 param location string = resourceGroup().location
-param tenantId string
 param authAuthority string
 param authAudience string
 param authValidIssuer string
---
+++ deployment/secrets.ps1
@@ -0,0 +1,25 @@
+param ($keyVaultName)
+
+# Will expose method NewPassword
+. ./secretGenerator.ps1
+
+################################################################
+### Set Key Vault secrets to provided values.
+################################################################
+$secretName = "asadfads-asdadasdaaaaa"
+
+# We have to check whether all the relevant secrets are in there.
+# If not, generate those secrets and store in Key Vault.
+$query = "contains([].id, 'https://$($keyVaultName).vault.azure.net/secrets/$($secretName)')"
+$exists = az keyvault secret list --vault-name $keyVaultName --query $query
+if ($exists -eq "false") {
+	az keyvault secret set --vault-name $keyVaultName --name $secretName --value $(NewPassword)
+}
+
+$sqlSaAdminPassword = NewPassword
+# $sqlEntraAdminPassword = NewPassword
+# 
+Write-Host "sqlSaAdminPassword:: $sqlSaAdminPassword"
+Write-Host "sqlEntraAdminPassword:: $sqlEntraAdminPassword"
+# Write-Host "##vso[task.setvariable variable=sqlSaAdminPassword;isoutput=true]$sqlSaAdminPassword"
+# Write-Host "##vso[task.setvariable variable=sqlEntraAdminPassword;isoutput=true]$sqlEntraAdminPassword"
