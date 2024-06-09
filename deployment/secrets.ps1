46d4ba595e8e779e4537009edc54991ca316c72b
Nenad Livaic <nenad@revelator.com>
2024-06-09 18:16:36 +0200

    Sql Server passwords generated and stored to Key Vault. [+semver: minor]

--- deployment/secrets.ps1
+++ deployment/secrets.ps1
@@ -6,21 +6,21 @@ param ($keyVaultName)
 ################################################################
 ### Set Key Vault secrets to provided values.
 ################################################################
-$secretName = "secretFromKeyVault111"
+$sqlSaPasswordSecretName = "SQL_SA_PASSWORD"
+$sqlAdminPasswordSecretName = "SQL_ADMIN_PASSWORD"
 
 # We have to check whether all the relevant secrets are in there.
 # If not, generate those secrets and store in Key Vault.
-$query = "contains([].id, 'https://$($keyVaultName).vault.azure.net/secrets/$($secretName)')"
+$query = "contains([].id, 'https://$($keyVaultName).vault.azure.net/secrets/$($sqlSaPasswordSecretName)')"
 $exists = az keyvault secret list --vault-name $keyVaultName --query $query
 if ($exists -eq "false") {
-	az keyvault secret set --vault-name $keyVaultName --name $secretName --value $(NewPassword)
-	Write-Host "##[section]Set secret $secretName"
+	az keyvault secret set --vault-name $keyVaultName --name $sqlSaPasswordSecretName --value $(NewPassword)
+	Write-Host "##[section]Set secret $sqlSaPasswordSecretName"
 }
 
-$sqlSaAdminPassword = NewPassword
-$sqlEntraAdminPassword = NewPassword
-
-Write-Host "sqlSaAdminPassword:: $sqlSaAdminPassword"
-Write-Host "sqlEntraAdminPassword:: $sqlEntraAdminPassword"
-Write-Host "##vso[task.setvariable variable=sqlSaAdminPassword;isoutput=true]$sqlSaAdminPassword"
-Write-Host "##vso[task.setvariable variable=sqlEntraAdminPassword;isoutput=true]$sqlEntraAdminPassword"
+$query = "contains([].id, 'https://$($keyVaultName).vault.azure.net/secrets/$($sqlAdminPasswordSecretName)')"
+$exists = az keyvault secret list --vault-name $keyVaultName --query $query
+if ($exists -eq "false") {
+	az keyvault secret set --vault-name $keyVaultName --name $sqlAdminPasswordSecretName --value $(NewPassword)
+	Write-Host "##[section]Set secret $sqlAdminPasswordSecretName"
+}
