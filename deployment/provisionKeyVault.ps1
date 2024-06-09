59a1e6f83151e9590b8b0ae3199f088471f04982
Nenad Livaic <nenad@revelator.com>
2024-06-09 16:20:19 +0200

    Adding 'Key Vault Secrets Officer' role to Azure Connection. [+semver: minor] [+semver: minor] [+semver: minor]

--- deployment/provisionKeyVault.ps1
+++ deployment/provisionKeyVault.ps1
@@ -1,8 +1,24 @@
 param ($environment, $projectName, $resourceGroupName)
 
-Write-Host "##[warning]------ Create Key Vault Resource START ------"
+$rgId = az group show --resource-group $resourceGroupName --query id --output tsv
+####################################################
+### Create Key Vault 
+####################################################
+Write-Host "##[warning]------ Create Key Vault START ------"
 $tenantId = (az account list --query "[?isDefault].tenantId | [0]" --output tsv)
 $jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file ./deployment/iacKeyVault.bicep --parameters environment=$environment projectName=$projectName tenantId=$tenantId | ConvertFrom-Json
 $keyVaultName = $jsonResultAll.properties.outputs.keyVaultName.value
 Write-Host "##vso[task.setvariable variable=keyVaultName;isoutput=true]$keyVaultName"
-Write-Host "##[warning]------ Create Key Vault Resource END ------"
+Write-Host "##[warning]------ Create Key Vault END ------"
+
+####################################################
+### Assign Key Vault Roles to AzureConnection
+####################################################
+Write-Host "##[warning]------ Assign Key Vault Roles to AzureConnection START ------"
+$servicePrincipalId = az ad sp list --filter "appId eq '$env:servicePrincipalId'" --query '[].id' --output tsv
+# "Key Vault Secrets Officer" role
+echo "FOO FOO FOO FOO FOO FOO"
+echo $servicePrincipalId
+echo "FOO FOO FOO FOO FOO FOO"
+az role assignment create --assignee $servicePrincipalId --role "b86a8fe4-44ce-4948-aee5-eccb2c155cd7" --scope $rgId
+Write-Host "##[warning]------ Assign Key Vault Roles to AzureConnection END ------"
--- deployment/secrets.ps1
+++ deployment/secrets.ps1
@@ -6,7 +6,7 @@ param ($keyVaultName)
 ################################################################
 ### Set Key Vault secrets to provided values.
 ################################################################
-$secretName = "secretFromKeyVault"
+$secretName = "secretFromKeyVault111"
 
 # We have to check whether all the relevant secrets are in there.
 # If not, generate those secrets and store in Key Vault.
@@ -14,12 +14,13 @@ $query = "contains([].id, 'https://$($keyVaultName).vault.azure.net/secrets/$($s
 $exists = az keyvault secret list --vault-name $keyVaultName --query $query
 if ($exists -eq "false") {
 	az keyvault secret set --vault-name $keyVaultName --name $secretName --value $(NewPassword)
+	Write-Host "##[section]Set secret $secretName"
 }
 
 $sqlSaAdminPassword = NewPassword
 $sqlEntraAdminPassword = NewPassword
-Write-Host "sqlSaAdminPassword:: $sqlSaAdminPassword"
 
+Write-Host "sqlSaAdminPassword:: $sqlSaAdminPassword"
 Write-Host "sqlEntraAdminPassword:: $sqlEntraAdminPassword"
 Write-Host "##vso[task.setvariable variable=sqlSaAdminPassword;isoutput=true]$sqlSaAdminPassword"
 Write-Host "##vso[task.setvariable variable=sqlEntraAdminPassword;isoutput=true]$sqlEntraAdminPassword"
