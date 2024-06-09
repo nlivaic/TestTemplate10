df2df3fcad3bc29823a9f33602627bf1054f583c
Nenad Livaic <nenad@revelator.com>
2024-06-09 14:03:55 +0200

    Echo key vault name from pipeline. [+semver: minor] [+semver: minor]

--- deployment/provisionKeyVault.ps1
+++ deployment/provisionKeyVault.ps1
@@ -3,6 +3,6 @@ param ($environment, $projectName, $resourceGroupName)
 Write-Host "##[warning]------ Create Key Vault Resource START ------"
 $tenantId = (az account list --query "[?isDefault].tenantId | [0]" --output tsv)
 $jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file ./deployment/iacKeyVault.bicep --parameters environment=$environment projectName=$projectName tenantId=$tenantId | ConvertFrom-Json
-$keyVaultName = $jsonResultAll.properties.outputs.$keyVaultName.value
+$keyVaultName = $jsonResultAll.properties.outputs.keyVaultName.value
 Write-Host "##vso[task.setvariable variable=keyVaultName;isoutput=true]$keyVaultName"
 Write-Host "##[warning]------ Create Key Vault Resource END ------"
